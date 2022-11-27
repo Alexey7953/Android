f483b62abbfcdaa68dc136c9eddd4c6da59fcda6
Alexey Zakharov <zakharov_alexey@mail.ru>
2022-11-28 00:00:50 +0500

    Initial commit

---
+++ Lesson_1/untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Program for calculating the volume of a parallelepipid:")
+    println()
+    println("Enter the length:")
+    val lenght: Int = readln().toInt()
+    println("Enter Width:")
+    val high: Int = readln().toInt()
+    println("Enter High:")
+    val widht: Int = readln().toInt()
+    println("Total:")
+    println(lenght*widht*high)
+}
